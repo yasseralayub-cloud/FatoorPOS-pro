9006a3ae32c0121e74e458812a53a10a552a28bf
yasseralayub-cloud <yasseralayub@gmail.com>
2025-11-30 09:30:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,12 @@
-// placeholder
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
